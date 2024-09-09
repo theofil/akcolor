a4de55d17b05027a42ace161c3679c056d8947c4
Konstantinos Theofilatos <konstantinos.theofilatos@gmail.com>
2024-09-09 19:51:27 +0200

    updating tot skip for job sub

--- makefriends/makefriends.py
+++ makefriends/makefriends.py
@@ -442,7 +442,7 @@ if __name__ == "__main__":
         for iev, event in enumerate(ttree):
             # skip events
             if iev < args.skip: continue
-            if args.totEve != -1 and (iev > args.skip + args.totEve): break
+            if args.totEve != -1 and (iev >= args.skip + args.totEve): break
 
             if args.goFast and iev >=  args.goFast*ttree.GetEntries()  : break
             if minimalPrint and iev%10000 == 0: print('event %d of %s'%(iev, tfiles[ii].GetName()))
@@ -507,16 +507,16 @@ if __name__ == "__main__":
                     momNOcluster.append(momenta[mm])
 
             # Phase-II offline cuts
-            # jcPtMin, jcEtaMax      = 2.0, 4.0
-            # jetPtMin, jetEtaMax    = 30.0, 3.0
+            jcPtMin, jcEtaMax      = 2.0, 4.0
+            jetPtMin, jetEtaMax    = 30.0, 3.0
 
             # Run2/3 offline cuts
             # jcPtMin, jcEtaMax      = 0.5, 2.4 
             # jetPtMin, jetEtaMax    = 30.0, 2.0   
 
             # Phase-II no cuts
-            jcPtMin, jcEtaMax      = 0.0, 10.0
-            jetPtMin, jetEtaMax    = 30.0, 3.0
+            # jcPtMin, jcEtaMax      = 0.0, 10.0
+            # jetPtMin, jetEtaMax    = 30.0, 3.0
 
             # Phase-II L1 cuts
             # jcPtMin, jcEtaMax      = 2.0, 2.4
