10cc44943d809db285c38e230822d2da33933407
Konstantinos Theofilatos <konstantinos.theofilatos@gmail.com>
2024-11-25 11:05:48 +0100

    real nocuts

--- makefriends/makefriends.py
+++ makefriends/makefriends.py
@@ -530,16 +530,16 @@ if __name__ == "__main__":
                     momNOcluster.append(momenta[mm])
 
             # Phase-II offline cuts
-            jcPtMin, jcEtaMax      = 2.0, 4.0
-            jetPtMin, jetEtaMax    = 30.0, 3.0
+            # jcPtMin, jcEtaMax      = 2.0, 4.0
+            # jetPtMin, jetEtaMax    = 30.0, 3.0
 
             # Run2/3 offline cuts
             # jcPtMin, jcEtaMax      = 0.5, 2.4 
             # jetPtMin, jetEtaMax    = 30.0, 2.0   
 
             # Phase-II no cuts
-            # jcPtMin, jcEtaMax      = 0.0, 10.0
-            # jetPtMin, jetEtaMax    = 30.0, 3.0
+            jcPtMin, jcEtaMax      = 0.0, 10.0
+            jetPtMin, jetEtaMax    = 30.0, 3.0
 
             # Phase-II L1 cuts
             # jcPtMin, jcEtaMax      = 2.0, 2.4
